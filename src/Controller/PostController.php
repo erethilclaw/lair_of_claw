b2bdc5485b951183cc7e350985abc12108d5c311
Ernest Riccetto <ernest.riccetto@gmail.com>
2019-01-22 10:57:27 +0100

    basic persist new Post and list

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,16 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Form\PostFormType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends AbstractController {
 
-	public function new(){
+	public function newPost(EntityManagerInterface $em, Request $request){
 		$form = $this->createForm(PostFormType::class);
+		$form->handleRequest($request);
+		if ($form->isSubmitted() && $form->isValid()) {
+			$post = new Post();
+			$post = $form->getData();
+			//toremove
+			$post->setSlug('puta');
+			$em->persist($post);
+			$em->flush();
+
+			return $this->redirectToRoute('listPost');
+		}
 
 		return $this->render('post/newPost.html.twig', [
 			'postForm' => $form->createView(),
+		]);
+	}
+
+	public function listPost(PostRepository $post_repository){
+		$posts = $post_repository->findAll();
+
+		return $this->render('post/listPost.html.twig', [
+			'posts'=>$posts,
 		]);
 	}
 }
--- src/Form/PostFormType.php
+++ src/Form/PostFormType.php
@@ -2,15 +2,25 @@
 
 namespace App\Form;
 
+use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PostFormType extends AbstractType {
 
 	public function buildForm( FormBuilderInterface $builder, array $options ) {
 		$builder
-			->add('title')
-			->add('content')
+			->add('title', TextType::class)
+			->add('content', TextareaType::class)
 			;
+	}
+
+	public function configureOptions( OptionsResolver $resolver ) {
+		$resolver->setDefaults([
+			'data_class'=> Post::class,
+		]);
 	}
 }
