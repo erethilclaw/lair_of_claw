2732e378827969659444d9d4e3560d040c0c33dd
Ernest Riccetto <ernest.riccetto@gmail.com>
2019-01-31 15:47:35 +0100

    delete kabooum

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -6,27 +6,15 @@ use App\Entity\Post;
 use App\Form\PostFormType;
 use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class PostController extends AbstractController {
-
-	public function newPost(EntityManagerInterface $em, Request $request){
-		$form = $this->createForm(PostFormType::class);
-		$form->handleRequest($request);
-		if ($form->isSubmitted() && $form->isValid()) {
-			$post = $form->getData();
-			$em->persist($post);
-			$em->flush();
-			$this->addFlash('success', 'Article Created! Knowledge is power!');
-
-			return $this->redirectToRoute('listPost');
-		}
-
-		return $this->render('post/newPost.html.twig', [
-			'postForm' => $form->createView(),
-		]);
-	}
 
 	public function listPost(PostRepository $post_repository, $page = 1, Request $request){
 		$limit = $request->get('limit', 10);
@@ -56,10 +44,27 @@ class PostController extends AbstractController {
 		);
 	}
 
-	public function viewPost($slug){
-		$post = $this->searchPostBySlug($slug);
+	/**
+	 * @Route("/admin/post/{slug}", name="viewPost", methods={"GET"})
+	 */
+	public function viewPost(Post $post){
 		return $this->json($post);
 	}
+
+	/**
+	 * @Route("admin/newPost", name="newPost", methods={"POST"})
+	 */
+	public function newPost( Request $request){
+		$serializar = $this->get('serializer');
+		$post = $serializar->deserialize($request->getContent(), Post::class,'json');
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($post);
+		$em->flush();
+
+		return $this->json($post);
+	}
+
 
 	public function editPost($slug, EntityManagerInterface $em, Request $request){
 		$post = $this->searchPostBySlug($slug);
@@ -79,21 +84,15 @@ class PostController extends AbstractController {
 		]);
 	}
 
-	public function deletePost($slug, EntityManagerInterface $em){
-		$post = $this->searchPostBySlug($slug);
-		if (!$post) {
-            throw $this->createNotFoundException("Destinatari no trobat");
-        }
+	/**
+	 * @Route("/admin/post/{slug}, name="deletePost", methods={"DELETE"})
+	 */
+	public function deletePost(Post $post){
+		$em = $this->getDoctrine()->getManager();
         $em->remove($post);
 		$em->flush();
-		$this->addFlash('success', 'Article deleted!');
-		return $this->redirectToRoute('listPost');
+
+		return new JsonResponse(null,Response::HTTP_NO_CONTENT);
 	}
 
-	public function searchPostBySlug($slug){
-		$post_repository = $this->getDoctrine()->getRepository(Post::class);
-		return $post = $post_repository->findOneBy([
-			'slug'=>$slug
-		]);
-	}
 }
